c6085ea7b08bd17f3d8eb301bd7a4fe7548e6971
Francesca Capuzzo <f.capuzzo9966@gmail.com>
2022-03-05 11:46:43 +0100

    header, footer, nav bar, images added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
@@ -13,6 +14,7 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    NavBarComponent,
     WelcomeComponent
   ],
   imports: [
