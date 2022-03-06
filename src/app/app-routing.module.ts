568a4b43582db27813db5f821f2065be5d0acaae
nicologiuggiola <93972864+nicologiuggiola@users.noreply.github.com>
2022-03-06 13:35:22 +0100

    push 3 nic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TheBandComponent } from './components/the-band/the-band.component';
+import { ToursComponent } from './components/tours/tours.component';
+import { MediaComponent } from './components/media/media.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: WelcomeComponent },
+  { path: 'theband', component: TheBandComponent },
+  { path: 'tours', component: ToursComponent },
+  { path: 'media', component: MediaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
